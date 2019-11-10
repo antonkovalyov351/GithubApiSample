5520d27276e5cb69ac7d60ca1220db9024ee2c2c
Anton Kovalyov <11294259+antonkovalyov351@users.noreply.github.com>
2019-11-10 17:48:09 +0200

    Add api test

---
+++ app/src/test/java/com/example/githubapisample/api/GithubApiServiceTest.kt
@@ -0,0 +1,62 @@
+package com.example.githubapisample.api
+
+import com.example.githubapisample.data.net.GithubApiService
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.Okio
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+@RunWith(JUnit4::class)
+class GithubApiServiceTest {
+
+    private lateinit var service: GithubApiService
+
+    private lateinit var mockWebServer: MockWebServer
+
+    @Before
+    fun createService() {
+        mockWebServer = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(GithubApiService::class.java)
+    }
+
+    @After
+    fun stopService() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun search() {
+        enqueueResponse("search.json")
+
+        val response = service.searchRepos("jetpack").blockingGet()
+
+        assertThat(response, notNullValue())
+        assertThat(response.total, `is`(3367))
+        assertThat(response.items.size, `is`(30))
+    }
+
+    private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = Okio.buffer(Okio.source(inputStream))
+        val mockResponse = MockResponse()
+        for ((key, value) in headers) {
+            mockResponse.addHeader(key, value)
+        }
+        mockWebServer.enqueue(mockResponse.setBody(source.readString(Charsets.UTF_8)))
+    }
+}
